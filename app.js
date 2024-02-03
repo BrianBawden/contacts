bf8a2fee63b41278dd728300ab467a026d5e960a
Brian Bawden <brianbawden001@gmail.com>
2024-02-03 14:00:45 -0500

    try cors again.

--- app.js
+++ app.js
@@ -1,24 +1,16 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongodb = require('./db/connect')
-// const cors = require('cors')
+const cors = require('cors')
 
 const port = process.env.PORT || 8080
 const app = express()
 
 app 
   .use(bodyParser.json())
-  // .use(cors({
-  //   origin: "*"
-  // }))
+  .use(cors())
   .use((req, res, next) =>{
-    res.setHeader('Acess-Control-Allow-Origin', '*')
-    // res.setHeader(
-    //   'Access-Control-Allow-Headers',
-    //   'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
-    // );
-    // res.setHeader('Content-Type', 'application/json');
-    // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+    res.setHeader('Access-Control-Allow-Origin', '*')
     next()
   })
   .use('/', require('./routes'))
